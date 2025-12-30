e2002550b0efab0acafa1eba875f4e4ae5b5d506
masato-N918 <b152122y@gmail.com>
2025-12-30 19:46:03 +0900

    refactor: 表記の変更
    - _による区切りに統一
    - キャメルケースをスネークケースに変更

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -8,43 +8,43 @@ resource "aws_vpc" "main" {
 
 ## Create Public and Private Subnets in ap-northeast-1a and ap-northeast-1c
 
-resource "aws_subnet" "public-1a" {
+resource "aws_subnet" "public_1a" {
   vpc_id = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "ap-northeast-1a"
 
   tags = {
-    Name = "public-subnet-1a"
+    Name = "public_subnet_1a"
   }
 }
 
-resource "aws_subnet" "public-1c" {
+resource "aws_subnet" "public_1c" {
   vpc_id = aws_vpc.main.id
   cidr_block = "10.0.3.0/24"
   availability_zone = "ap-northeast-1c"
 
   tags = {
-    Name = "public-subnet-1c"
+    Name = "public_subnet_1c"
   }
 }
 
-resource "aws_subnet" "private-1a" {
+resource "aws_subnet" "private_1a" {
   vpc_id = aws_vpc.main.id
   cidr_block = "10.0.10.0/24"
   availability_zone = "ap-northeast-1a"
 
   tags = {
-    Name = "private-subnet-1a"
+    Name = "private_subnet_1a"
   }
 }
 
-resource "aws_subnet" "private-1c" {
+resource "aws_subnet" "private_1c" {
   vpc_id = aws_vpc.main.id
   cidr_block = "10.0.12.0/24"
   availability_zone = "ap-northeast-1c"
 
   tags = {
-    Name = "private-subnet-1c"
+    Name = "private_subnet_1c"
   }
 }
 
@@ -54,11 +54,11 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
   tags = {
-    Name = "main-igw"
+    Name = "main_igw"
   }
 }
 
-resource "aws_route_table" "public-rt" {
+resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main.id
 
   route {
@@ -66,89 +66,89 @@ resource "aws_route_table" "public-rt" {
     gateway_id = aws_internet_gateway.igw.id
   }
     tags = {
-        Name = "public-rt"
+        Name = "public_rt"
     }
 }
 
-resource "aws_route_table_association" "public-1a-assoc" {
-  subnet_id      = aws_subnet.public-1a.id
-  route_table_id = aws_route_table.public-rt.id
+resource "aws_route_table_association" "public_1a_assoc" {
+  subnet_id      = aws_subnet.public_1a.id
+  route_table_id = aws_route_table.public_rt.id
 }
 
-resource "aws_route_table_association" "public-1c-assoc" {
-  subnet_id      = aws_subnet.public-1c.id
-  route_table_id = aws_route_table.public-rt.id
+resource "aws_route_table_association" "public_1c_assoc" {
+  subnet_id      = aws_subnet.public_1c.id
+  route_table_id = aws_route_table.public_rt.id
 }
 
 ## Create Private Route Table and NAT Gateways
 
-resource "aws_route_table" "private-rt-1a" {
+resource "aws_route_table" "private_rt_1a" {
   vpc_id = aws_vpc.main.id
 
     tags = {
-        Name = "private-rt-1a"
+        Name = "private_rt_1a"
     }
 }
-resource "aws_route_table" "private-rt-1c" {
+resource "aws_route_table" "private_rt_1c" {
   vpc_id = aws_vpc.main.id
 
     tags = {
-        Name = "private-rt-1c"
+        Name = "private_rt_1c"
     }
 }
 
-resource "aws_route" "private-rt-1a-nat-route" {
-  route_table_id         = aws_route_table.private-rt-1a.id
+resource "aws_route" "private_rt_1a_nat_route" {
+  route_table_id         = aws_route_table.private_rt_1a.id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.nat-gw-1a.id
+  nat_gateway_id         = aws_nat_gateway.nat_gw_1a.id
 }
 
-resource "aws_route" "private-rt-1c-nat-route" {
-  route_table_id         = aws_route_table.private-rt-1c.id
+resource "aws_route" "private_rt_1c_nat_route" {
+  route_table_id         = aws_route_table.private_rt_1c.id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.nat-gw-1c.id
+  nat_gateway_id         = aws_nat_gateway.nat_gw_1c.id
 }
 
-resource "aws_route_table_association" "private-1a-assoc" {
-  subnet_id      = aws_subnet.private-1a.id
-  route_table_id = aws_route_table.private-rt-1a.id
+resource "aws_route_table_association" "private_1a_assoc" {
+  subnet_id      = aws_subnet.private_1a.id
+  route_table_id = aws_route_table.private_rt_1a.id
 }
 
-resource "aws_route_table_association" "private-1c-assoc" {
-  subnet_id      = aws_subnet.private-1c.id
-  route_table_id = aws_route_table.private-rt-1c.id
+resource "aws_route_table_association" "private_1c_assoc" {
+  subnet_id      = aws_subnet.private_1c.id
+  route_table_id = aws_route_table.private_rt_1c.id
 }
 
-resource "aws_nat_gateway" "nat-gw-1a" {
-  allocation_id = aws_eip.nat-eip-1a.id
-  subnet_id     = aws_subnet.public-1a.id
+resource "aws_nat_gateway" "nat_gw_1a" {
+  allocation_id = aws_eip.nat_eip_1a.id
+  subnet_id     = aws_subnet.public_1a.id
 
   tags = {
-    Name = "nat-gw-1a"
+    Name = "nat_gw_1a"
   }
 }
-resource "aws_eip" "nat-eip-1a" {
+resource "aws_eip" "nat_eip_1a" {
     domain = "vpc"
 
   tags = {
-    Name = "nat-eip-1a"
+    Name = "nat_eip_1a"
   }
 }
 
-resource "aws_nat_gateway" "nat-gw-1c" {
-  allocation_id = aws_eip.nat-eip-1c.id
-  subnet_id     = aws_subnet.public-1c.id
+resource "aws_nat_gateway" "nat_gw_1c" {
+  allocation_id = aws_eip.nat_eip_1c.id
+  subnet_id     = aws_subnet.public_1c.id
 
   tags = {
-    Name = "nat-gw-1c"
+    Name = "nat_gw_1c"
   }
 }
 
-resource "aws_eip" "nat-eip-1c" {
+resource "aws_eip" "nat_eip_1c" {
     domain = "vpc"
 
   tags = {
-    Name = "nat-eip-1c"
+    Name = "nat_eip_1c"
   }
 }
 
--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -5,17 +5,17 @@ output "vpc_id" {
 
 output "public_subnet_ids" {
   description = "List of public subnet IDs"
-  value       = [aws_subnet.public-1a.id, aws_subnet.public-1c.id]
+  value       = [aws_subnet.public_1a.id, aws_subnet.public_1c.id]
 }
 
 output "private_subnet_ids" {
   description = "List of private subnet IDs"
-  value       = [aws_subnet.private-1a.id, aws_subnet.private-1c.id]
+  value       = [aws_subnet.private_1a.id, aws_subnet.private_1c.id]
 }
 
 output "nat_gateway_ids" {
   description = "List of NAT Gateway IDs"
-  value       = [aws_nat_gateway.nat-gw-1a.id, aws_nat_gateway.nat-gw-1c.id]
+  value       = [aws_nat_gateway.nat_gw_1a.id, aws_nat_gateway.nat_gw_1c.id]
 }
 
 output "internet_gateway_id" {
@@ -25,10 +25,10 @@ output "internet_gateway_id" {
 
 output "public_route_table_id" {
   description = "The ID of the Public Route Table"
-  value       = aws_route_table.public-rt.id
+  value       = aws_route_table.public_rt.id
 }
 
 output "private_route_table_ids" {
   description = "List of Private Route Table IDs"
-  value       = [aws_route_table.private-rt-1a.id, aws_route_table.private-rt-1c.id]
+  value       = [aws_route_table.private_rt_1a.id, aws_route_table.private_rt_1c.id]
 }
