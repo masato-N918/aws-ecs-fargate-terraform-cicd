4e1e714d1252a2e0e03c6ed6879d7d198e78dc7c
masato-N918 <b152122y@gmail.com>
2026-01-01 08:22:45 +0900

    style: 表記の修正

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -23,7 +23,7 @@ resource "aws_vpc_security_group_egress_rule" "ecs_sg_egress" {
   from_port        = 0
   to_port          = 0
   ip_protocol      = "-1"
-  cidr_ipv4     = ["0.0.0.0/0"]
+  cidr_ipv4        = "0.0.0.0/0"
 }
 
 # ECS Task Definition
