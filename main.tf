99fce9eb03da351b3fcb048acd040e16cdc5be30
masato-N918 <b152122y@gmail.com>
2025-12-31 11:07:13 +0900

    feat: main.tfファイルを追加
    - VPC,ALB,ECR,ECSのモジュールを定義
    - 各モジュール間の依存関係を設定

---
+++ main.tf
@@ -0,0 +1,24 @@
+module "vpc" {
+  source = "./modules/vpc"
+}
+
+module "alb" {
+  source = "./modules/alb"
+  vpc_id = module.vpc.vpc_id
+  public_subnet_ids = module.vpc.public_subnet_ids
+}
+
+module "ecr" {
+  source = "./modules/ecr"
+}
+
+module "ecs" {
+  source = "./modules/ecs"
+  aws_region = "ap-northeast-1"
+  vpc_id = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnet_ids
+  alb_security_group_id = module.alb.alb_security_group_id
+  alb_target_group_arn = module.alb.alb_target_group_arn
+  sidecar_image_url = module.ecr.ecr_repository_url
+
+}
