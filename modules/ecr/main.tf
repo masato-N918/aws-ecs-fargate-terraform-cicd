72c291474b9024595a310e2c4dd291322032b18a
masato-N918 <b152122y@gmail.com>
2026-01-12 10:33:45 +0900

    fix: ECRのライフサイクルポリシー修正

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -24,7 +24,7 @@ resource "aws_ecr_lifecycle_policy" "sidecar_lifecycle" {
         description  = "Expire old images"
         selection    = {
           tagStatus    = "tagged"
-          tagPrefixList = [""]
+          tagPrefixList = ["latest"]
           countType    = "imageCountMoreThan"
           countNumber  = 3
         }
