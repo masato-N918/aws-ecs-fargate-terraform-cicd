7ac4af66305c13c2e4e63a91ed522bcb5ea937db
masato-N918 <b152122y@gmail.com>
2026-01-31 14:15:30 +0900

    Format main.tf with terraform fmt

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ module "vpc" {
 }
 
 module "alb" {
-  source = "./modules/alb"
-  vpc_id = module.vpc.vpc_id
+  source            = "./modules/alb"
+  vpc_id            = module.vpc.vpc_id
   public_subnet_ids = module.vpc.public_subnet_ids
 }
 
@@ -13,20 +13,20 @@ module "ecr" {
 }
 
 module "ecs" {
-  source = "./modules/ecs"
-  aws_region = "ap-northeast-1"
-  vpc_id = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnet_ids
+  source                = "./modules/ecs"
+  aws_region            = "ap-northeast-1"
+  vpc_id                = module.vpc.vpc_id
+  subnet_ids            = module.vpc.private_subnet_ids
   alb_security_group_id = module.alb.alb_security_group_id
-  alb_target_group_arn = module.alb.alb_target_group_arn
-  sidecar_image_url = module.ecr.repository_url
+  alb_target_group_arn  = module.alb.alb_target_group_arn
+  sidecar_image_url     = module.ecr.repository_url
 }
 
 module "rds" {
-  source = "./modules/rds"
-  vpc_id = module.vpc.vpc_id
-  db_subnet_ids = module.vpc.private_subnet_ids
+  source                = "./modules/rds"
+  vpc_id                = module.vpc.vpc_id
+  db_subnet_ids         = module.vpc.private_subnet_ids
   ecs_security_group_id = module.ecs.ecs_security_group_id
-  db_username = var.db_username
-  db_password = var.db_password
+  db_username           = var.db_username
+  db_password           = var.db_password
 }
